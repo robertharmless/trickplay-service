2b6e6f1ee4a994f9f32bf775012b0e42151721d7
robertharmless <88203930+robertharmless@users.noreply.github.com>
2021-08-27 21:31:31 -0700

    Updated the app with basic configs

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 """
 Simple app to create Trick Play - Mode from/for HLS m3u8.
 """
+# Built-in
 
+
+# Special
 from flask import Flask
+
+# App
+from api.event import post_event
+from api.log_listener import setup_log_event_handlers
+
+setup_log_event_handlers()
 
 app = Flask(__name__)
 
@@ -10,3 +19,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+#TODO: Add catching errors
+#TODO: Convert to restful focus
+#TODO: Proper validation could be helpful. Parsing paramters?
+
+#TODO: Point to trick play-mode
+
+#TODO: Able to work with multiple storage methods. Though ffmpeg may require being local.
+
+#TODO: Mesasure progress for insights?
+#TODO: Add Postman Collection
+#TODO: Add Open API documentation
+
